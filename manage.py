8dad9781496bfa447b6a261987e2ea3cdf47cd7b
kurtgalvin <45803439+kurtgalvin@users.noreply.github.com>
2019-11-28 18:13:35 -0800

    add post environ variable

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 import sys
+import os
 from app import app
 
 args = sys.argv[1:]
 
 if "runserver" in args:
-    app.run(host="0.0.0.0", port='5001', debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get('PORT', '5001'), debug=True)
